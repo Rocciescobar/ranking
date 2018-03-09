016f41fe5f0140e8313fdabc3c308b588c672af3
MarinaRh <l_marina1717@hotmail.com>
2018-03-09 17:53:38 -0500

    Add section Reproducer

--- src/components/app.js
+++ src/components/app.js
@@ -1,61 +1,27 @@
 import React from 'react'
 import logo from '../logo.png'
-// import next from '../lef.png'
+import next from '../lef.png'
+import preview from '../rigth.png'
 import './App.css'
 
 
-{
-  const songs= [
-    {
-      id: 1,
-      song: 'canciónA',
-      autor: 'autorA',
-      score: 30
-    },
-    {
-      id: 2,
-      song: 'canciónB',
-      autor: 'autorB',
-      score: 33
-    },
-    {
-      id: 3,
-      song: 'canciónC',
-      autor: 'autorC',
-      score: 23
-    },
-    {
-      id: 4,
-      song: 'canciónD',
-      autor: 'autorD',
-      score: 10
-    },
-    {
-      id: 5,
-      song: 'canciónE',
-      autor: 'autorE',
-      score: 3
-    },
-    {
-      id: 6,
-      song: 'canciónF',
-      autor: 'autorF',
-      score: 13
-    },
-  ];
-  currentID: 2
-}
-
 const App = () => (
-  <div>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Ranking Music</h1>
-      </header>
-    </div>
-    <div className="reproducer">
-
+  <div className="App Center">
+    <header className="App-header">
+      <img src={logo} className="App-logo"/>
+      <h1 className="App-title">Ranking Music</h1>
+    </header>
+    <div className="Play">
+      <div className="Inline Next">
+        <button><img src={next}/></button>
+      </div>
+      <div className="Reproducer Inline"> 
+        <h2>Title</h2>
+        <p>Autor</p>
+      </div>
+      <div className="Inline Preview">
+        <button><img src={preview}/></button>
+      </div>
     </div>
   </div>
 )
