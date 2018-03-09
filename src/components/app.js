e6837f853a08df8f79b90d4cb4b76b715b9dd91e
rocciescobar <roccisen.88@gmail.com>
2018-03-09 11:44:54 -0500

    Añade JSON de state

---
+++ src/components/app.js
@@ -0,0 +1,43 @@
+// state 
+
+{
+  songs: [
+    {
+      id: 1,
+      song: 'canciónA',
+      autor: 'autorA',
+      score: 30
+    },
+    {
+      id: 2,
+      song: 'canciónB',
+      autor: 'autorB',
+      score: 33
+    },
+    {
+      id: 3,
+      song: 'canciónC',
+      autor: 'autorC',
+      score: 23
+    },
+    {
+      id: 4,
+      song: 'canciónD',
+      autor: 'autorD',
+      score: 10
+    },
+    {
+      id: 5,
+      song: 'canciónE',
+      autor: 'autorE',
+      score: 3
+    },
+    {
+      id: 6,
+      song: 'canciónF',
+      autor: 'autorF',
+      score: 13
+    },
+  ];
+  currentID: 2
+}
